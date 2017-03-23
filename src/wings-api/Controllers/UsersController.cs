6b3ae685954aa9a7a4b44be87f7bdda48e20d259
Roy Moran <rmoran20@uic.edu>
2017-03-22 18:37:37 -0700

    Add project files.

---
+++ src/wings-api/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wings_api.Models;
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace wings_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        User[] users = new User[]
+        {
+            new User { Id = 1, First_Name = "Roy", Last_Name = "Moran", Age = 23, School = "UIC", Job = "Consultant", Gender = "Male", Gender_Interested = "Female", Age_Interested_Min = 18, Age_Interested_Max = 22, Bio = "Why, hello there.", Loves_One = "Mother nature", Loves_Two = "One Direction", Loves_Three = "Nancy Pelosi", Onboarded = false},
+            new User { Id = 2, First_Name = "Aman", Last_Name = "Choudhury", Age = 24, School = "UIC", Job = "Finance", Gender = "Male", Gender_Interested = "Female", Age_Interested_Min = 18, Age_Interested_Max = 30, Bio = "What is love, baby don't hurt me. Don't hurt me. No more.", Loves_One = "World Travel", Loves_Two = "Lifting Heavy", Loves_Three = "Kanye West aka Yeezus", Onboarded = false}
+        };
+        
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return users;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = users.FirstOrDefault((p) => p.Id == id);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/wings-api/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wings_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Age { get; set; }
+        public string School { get; set; }
+        public string Job { get; set; }
+        public string Gender { get; set; } // male or female
+        public string Gender_Interested { get; set; } // male, female, or both
+        public int Age_Interested_Min { get; set; }
+        public int Age_Interested_Max { get; set; }
+        public string Bio { get; set; }
+        public string Loves_One { get; set; }
+        public string Loves_Two { get; set; }
+        public string Loves_Three { get; set; }
+        public bool Onboarded { get; set; }
+    }
+}
