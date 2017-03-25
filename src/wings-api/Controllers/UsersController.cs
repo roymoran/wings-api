dde55d3333cc4eacb977fb14562c60b01c503d7a
Roy Moran <rmoran20@uic.edu>
2017-03-25 00:57:07 -0700

    updated w/ user property names

--- src/wings-api/Controllers/UsersController.cs
+++ src/wings-api/Controllers/UsersController.cs
@@ -13,8 +13,8 @@ namespace wings_api.Controllers
     {
         User[] users = new User[]
         {
-            new User { Id = 1, First_Name = "Roy", Last_Name = "Moran", Age = 23, School = "UIC", Job = "Consultant", Gender = "Male", Gender_Interested = "Female", Age_Interested_Min = 18, Age_Interested_Max = 22, Bio = "Why, hello there.", Loves_One = "Mother nature", Loves_Two = "One Direction", Loves_Three = "Nancy Pelosi", Onboarded = false},
-            new User { Id = 2, First_Name = "Aman", Last_Name = "Choudhury", Age = 24, School = "UIC", Job = "Finance", Gender = "Male", Gender_Interested = "Female", Age_Interested_Min = 18, Age_Interested_Max = 30, Bio = "What is love, baby don't hurt me. Don't hurt me. No more.", Loves_One = "World Travel", Loves_Two = "Lifting Heavy", Loves_Three = "Kanye West aka Yeezus", Onboarded = false}
+            new User { Id = 1, FirstName = "Roy", LastName = "Moran", School = "UIC", Job = "Consultant", Gender = "Male", GenderInterested = "Female", AgeInterestedMin = 18, AgeInterestedMax = 22, Bio = "Why, hello there.", LovesOne = "Mother nature", LovesTwo = "One Direction", LovesThree = "Nancy Pelosi", Onboarded = false},
+            new User { Id = 2, FirstName = "Aman", LastName = "Choudhury", School = "UIC", Job = "Finance", Gender = "Male", GenderInterested = "Female", AgeInterestedMin = 18, AgeInterestedMax = 30, Bio = "What is love, baby don't hurt me. Don't hurt me. No more.", LovesOne = "World Travel", LovesTwo = "Lifting Heavy", LovesThree = "Kanye West aka Yeezus", Onboarded = false}
         };
         
         // GET: api/values
