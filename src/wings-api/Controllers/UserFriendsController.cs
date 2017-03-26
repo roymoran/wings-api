47bec632f7cad440fe4ab40bb490915d0eb36754
Roy Moran <rmoran20@uic.edu>
2017-03-26 12:40:07 -0700

    generated controller for each model

---
+++ src/wings-api/Controllers/UserFriendsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wings_api.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace wings_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserFriendsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<UserFriend> GetAllUserFriends()
+        {
+            UserFriend[] userFriends = new UserFriend[] {};
+            return userFriends;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
