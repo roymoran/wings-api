b508ea523543bf1511e49347e5ad696fbabc9c87
Roy Moran <rmoran20@uic.edu>
2017-04-01 00:11:25 -0700

    started implementation for posting user friend

--- src/wings-api/Controllers/UserFriendsController.cs
+++ src/wings-api/Controllers/UserFriendsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using wings_api.Data;
 using wings_api.Models;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,6 +13,13 @@ namespace wings_api.Controllers
     [Route("api/[controller]")]
     public class UserFriendsController : Controller
     {
+        private readonly WingsContext db;
+
+        public UserFriendsController(WingsContext context)
+        {
+            db = context;
+        }
+
         // GET: api/values
         [HttpGet]
         public IEnumerable<UserFriend> GetAllUserFriends()
@@ -29,8 +37,17 @@ namespace wings_api.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]UserFriend userFriend)
         {
+            // if UserFriendID exists we immediately save to database 
+            userFriend.DateCreated = DateTime.UtcNow;
+            db.Add(userFriend);
+            db.SaveChanges();
+            return Ok();
+
+            // otherwise we create a new user object and set active to false and save that UserFriendID
+            // value that must be filled in for new non-existent user friend is phone number to validate that
+            // that user friend joined on sign up
         }
 
         // PUT api/values/5
