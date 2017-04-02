41155798c6bce63e48daf797b8cd28435019e36e
Roy Moran <rmoran20@uic.edu>
2017-04-02 12:42:06 -0700

    Added DateUpdated property on user of type DateTime

--- src/wings-api/Models/User.cs
+++ src/wings-api/Models/User.cs
@@ -23,6 +23,7 @@ namespace wings_api.Models
         public string PhoneNumber { get; set; }
         public bool Active { get; set; }
         public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
         public string City { get; set; }
